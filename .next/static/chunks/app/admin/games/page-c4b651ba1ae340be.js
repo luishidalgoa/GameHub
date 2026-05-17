d7c1a5506d2559c459e647f4fa278dfd58ac1b8b
luishidalgoa <luishidalgoa@outlook.es>
2026-05-17 12:12:54 +0200

    feat: docker deployment config

--- .next/static/chunks/app/admin/games/page-c4b651ba1ae340be.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[508,931],{44193:function(n,e,u){Promise.resolve().then(u.t.bind(u,72972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=44193)}),_N_E=n.O()}]);
